97a322e6205150fb3e71c8dcb268c06593be7873
Görkem <gorkem_yelken@hotmail.com>
2023-01-23 19:19:43 +0300

    Initial commit

---
+++ src/main/java/com/definexjavaspringpracticum/finalcase/FinalcaseApplication.java
@@ -0,0 +1,13 @@
+package com.definexjavaspringpracticum.finalcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalcaseApplication.class, args);
+	}
+
+}
