217bbd0792e123272afc467c9ae5e7382f9e4ccb
Görkem <gorkem_yelken@hotmail.com>
2023-01-24 16:20:21 +0300

    Get all credit applications method is added.

--- src/main/java/com/definexjavaspringpracticum/finalcase/services/CreditApplicationService.java
+++ src/main/java/com/definexjavaspringpracticum/finalcase/services/CreditApplicationService.java
@@ -1,7 +1,37 @@
 package com.definexjavaspringpracticum.finalcase.services;
 
+import com.definexjavaspringpracticum.finalcase.modals.CreditApplication;
+import com.definexjavaspringpracticum.finalcase.modals.Customer;
+import com.definexjavaspringpracticum.finalcase.repositories.CreditApplicationRepository;
+import com.definexjavaspringpracticum.finalcase.responses.CreditApplicationResponse;
+import com.definexjavaspringpracticum.finalcase.responses.CustomerResponse;
+import com.definexjavaspringpracticum.finalcase.utilities.mapping.ModelMapperService;
+import com.definexjavaspringpracticum.finalcase.utilities.results.DataResult;
+import com.definexjavaspringpracticum.finalcase.utilities.results.SuccessDataResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CreditApplicationService {
+    Date date = Date.valueOf(LocalDate.now());
+    private CreditApplicationRepository creditApplicationRepository;
+    private ModelMapperService modelMapperService;
+
+    @Autowired
+    public CreditApplicationService(CreditApplicationRepository creditApplicationRepository, ModelMapperService modelMapperService) {
+        this.creditApplicationRepository = creditApplicationRepository;
+        this.modelMapperService = modelMapperService;
+    }
+
+    public DataResult<List<CreditApplicationResponse>> getAllCreditApplications() {
+        List<CreditApplication> creditApplications = this.creditApplicationRepository.findAll();
+        List<CreditApplicationResponse> result = creditApplications.stream().map(creditApplication -> this.modelMapperService.forDto().map(creditApplication,CreditApplicationResponse.class)).collect(Collectors.toList());
+        return new SuccessDataResult<>(result,"Credit applications are listed.");
+    }
+
 }
