63bd46d18e2fff1d92811f16cc9efc3f6da577b2
Görkem <gorkem_yelken@hotmail.com>
2023-02-07 12:29:18 +0300

    Credit Application Service Test is added.

--- src/test/java/com/definexjavaspringpracticum/finalcase/services/CreditApplicationServiceTest.java
+++ src/test/java/com/definexjavaspringpracticum/finalcase/services/CreditApplicationServiceTest.java
@@ -4,6 +4,7 @@ import com.definexjavaspringpracticum.finalcase.entities.CreditApplication;
 import com.definexjavaspringpracticum.finalcase.entities.Customer;
 import com.definexjavaspringpracticum.finalcase.repositories.CreditApplicationRepository;
 import com.definexjavaspringpracticum.finalcase.repositories.CustomerRepository;
+import com.definexjavaspringpracticum.finalcase.requests.CreditApplicationCreateRequest;
 import com.definexjavaspringpracticum.finalcase.responses.CreditApplicationResponse;
 import com.definexjavaspringpracticum.finalcase.utilities.mapping.ModelMapperService;
 import com.definexjavaspringpracticum.finalcase.utilities.results.DataResult;
@@ -41,18 +42,32 @@ public class CreditApplicationServiceTest {
     }
 
     @Test
-    public void createCreditApplication() {
+    public void testCreateCreditApplication() {
+        Customer customer = new Customer("12345678901", "TestFirstName", "TestLastName", 5000.0, "5555555555", Date.valueOf("1998-09-06"), 650);
+        CreditApplicationCreateRequest creditApplicationCreateRequest = new CreditApplicationCreateRequest(customer);
+
+        Mockito.when(customerRepository.existsByCustomerId(customer.getCustomerId())).thenReturn(true);
+
+        CreditApplication creditApplication = new CreditApplication(1L,"Approved.",10000.0,Date.valueOf("2023-02-07"),customer,5000.0,650);
+
+        Mockito.when(creditApplicationRepository.save(creditApplication)).thenReturn(creditApplication);
+
+        DataResult<CreditApplicationResponse> result = creditApplicationService.createCreditApplication(creditApplicationCreateRequest);
+
+        assertEquals("Credit application is added.", result.getMessage());
+        assertNotNull(result.getData());
     }
 
     @Test
-    public void delete() {
-    }
+    public void testDeleteCreditApplication() {
+        Customer customer = new Customer("12345678901", "TestFirstName", "TestLastName", 5000.0, "5555555555", Date.valueOf("1998-09-06"), 650);
+        CreditApplication creditApplication = new CreditApplication(1L,"Approved.",10000.0,Date.valueOf("2023-02-07"),customer,5000.0,650);
+        Long creditApplicationId = creditApplication.getCreditApplicationId();
 
-    @Test
-    public void find() {
-    }
+        Mockito.when(creditApplicationRepository.existsByCreditApplicationId(creditApplicationId)).thenReturn(true);
+        Mockito.when(creditApplicationRepository.findByCreditApplicationId(creditApplicationId)).thenReturn(creditApplication);
 
-    @Test
-    public void findByCreditApplicationId() {
+        DataResult<CreditApplicationResponse> result = creditApplicationService.delete(creditApplicationId);
+        assertEquals("Credit application is deleted.", result.getMessage());
     }
 }
