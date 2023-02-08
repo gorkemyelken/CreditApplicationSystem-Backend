545ece28c37adad1b89c6509735f3a719768a558
Görkem <gorkem_yelken@hotmail.com>
2023-02-08 10:55:04 +0300

    Code refactored.

--- src/main/java/com/definexjavaspringpracticum/finalcase/utilities/results/DataResult.java
+++ src/main/java/com/definexjavaspringpracticum/finalcase/utilities/results/DataResult.java
@@ -1,15 +1,35 @@
 package com.definexjavaspringpracticum.finalcase.utilities.results;
 
-public class DataResult<T> extends Result {
-    private final T data;
+public class DataResult<T>{
+    private T data;
+    private final boolean success;
+    private String message;
+
+    public DataResult(boolean success) {
+        this.success = success;
+    }
+
+    public DataResult(boolean success, String message) {
+        this(success);
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return this.success;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
 
     public DataResult(T data, boolean success, String message) {
-        super(success, message);
+        this.success = success;
+        this.message = message;
         this.data = data;
     }
 
     public DataResult(T data, boolean success) {
-        super(success);
+        this.success = success;
         this.data = data;
     }
 
