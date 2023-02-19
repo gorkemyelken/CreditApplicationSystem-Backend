98ee91108603c673efea528db283f9153149442c
Görkem <gorkem_yelken@hotmail.com>
2023-02-19 20:53:47 +0300

    CreditApplicationController log added.

--- src/main/java/com/definexjavaspringpracticum/finalcase/controllers/CreditApplicationController.java
+++ src/main/java/com/definexjavaspringpracticum/finalcase/controllers/CreditApplicationController.java
@@ -4,7 +4,8 @@ import com.definexjavaspringpracticum.finalcase.requests.CreditApplicationCreate
 import com.definexjavaspringpracticum.finalcase.responses.CreditApplicationResponse;
 import com.definexjavaspringpracticum.finalcase.services.CreditApplicationService;
 import com.definexjavaspringpracticum.finalcase.utilities.results.DataResult;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,36 +16,38 @@ import java.util.List;
 @RestController
 @RequestMapping("/creditapplications")
 @CrossOrigin
+@Slf4j
+@RequiredArgsConstructor
 public class CreditApplicationController {
     private final CreditApplicationService creditApplicationService;
 
-    @Autowired
-    public CreditApplicationController(CreditApplicationService creditApplicationService) {
-        this.creditApplicationService = creditApplicationService;
-    }
-
     @GetMapping
     public ResponseEntity<DataResult<List<CreditApplicationResponse>>> getAllCreditApplications(){
+        log.debug("[{}][createProduct] -> request: {}", this.getClass().getSimpleName(), "Get all credit applications.");
         return new ResponseEntity<>(this.creditApplicationService.getAllCreditApplications(), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<DataResult<CreditApplicationResponse>> createCreditApplication(@RequestBody CreditApplicationCreateRequest creditApplicationCreateRequest){
+        log.debug("[{}][createProduct] -> request: {}", this.getClass().getSimpleName(), "Create credit application.");
         return new ResponseEntity<>(this.creditApplicationService.createCreditApplication(creditApplicationCreateRequest),HttpStatus.CREATED );
     }
 
     @DeleteMapping("/{creditApplicationId}")
     public ResponseEntity<DataResult<CreditApplicationResponse>> deleteCreditApplication(@PathVariable Long creditApplicationId){
+        log.debug("[{}][createProduct] -> request: {}", this.getClass().getSimpleName(), "Delete credit application.");
         return new ResponseEntity<>(this.creditApplicationService.deleteCreditApplication(creditApplicationId),HttpStatus.OK);
     }
 
     @GetMapping("/find")
     public ResponseEntity<DataResult<List<Object>>> find(@RequestParam String identityNumber,Date birthDate){
+        log.debug("[{}][createProduct] -> request: {}", this.getClass().getSimpleName(), "Find by identity number and birth date.");
         return new ResponseEntity<>(this.creditApplicationService.find(identityNumber,birthDate),HttpStatus.OK);
     }
 
     @GetMapping("{creditApplicationId}")
     public ResponseEntity<DataResult<CreditApplicationResponse>> findByCreditApplicationId(@PathVariable Long creditApplicationId){
+        log.debug("[{}][createProduct] -> request: {}", this.getClass().getSimpleName(), "Find by credit application id.");
         return new ResponseEntity<>(this.creditApplicationService.findByCreditApplicationId(creditApplicationId), HttpStatus.OK);
     }
 }
